b8af0c4c41f003e57279da1060f762037dfecf6b
Intan9 <90880060+Intan9@users.noreply.github.com>
2021-11-18 22:05:15 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1> Halo, Perkenalkan</h1>"; <br>
+        echo "Nama saya Intan Restu Oktaviani. <br>
+        saya tinggal di daerah Ciawi.<br>
+         Olahraga yang saya sukai yaitu bermain badminton"; <br>
+        
+    }
+}
