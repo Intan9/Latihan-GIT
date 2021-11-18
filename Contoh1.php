567cbe54f4b511a52082c2ee9a34fa7cdcc5f493
Intan9 <90880060+Intan9@users.noreply.github.com>
2021-11-18 22:07:46 +0700

    Create Contoh1.php

---
+++ Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Intan Restu Oktaviani <br>
+        Saya tinggal di daerah Ciawi, Bogor. <br>
+        Olahraga yang saya suka yaitu Badminton dan berenang";
+    }
+}
